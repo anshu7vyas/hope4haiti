a8cdbdfa07cde64b4fb2165f591ef3ae5cc6b584
Ryan Hennings <r.hennings11@gmail.com>
2017-07-01 15:55:16 -0700

    collision detection

--- Scripts/hero.gd
+++ Scripts/hero.gd
@@ -7,24 +7,32 @@ var moving = false
 const SPEED = 1
 const GRID = 16
 
+var world
+
 func _ready():
+	world = get_world_2d().get_direct_space_state()
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	if !moving:
-		if Input.is_action_pressed("ui_up"):
+		var resultUp = world.intersect_point(get_pos() + Vector2(0, -GRID))
+		var resultDown = world.intersect_point(get_pos() + Vector2(0, GRID))
+		var resultLeft = world.intersect_point(get_pos() + Vector2(-GRID, 0))
+		var resultRight = world.intersect_point(get_pos() + Vector2(GRID, 0))
+		
+		if Input.is_action_pressed("ui_up") and resultUp.empty():
 			moving = true
 			direction = Vector2(0, -1)
 			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_down"):
+		elif Input.is_action_pressed("ui_down") and resultDown.empty():
 			moving = true
 			direction = Vector2(0, 1)
 			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_left"):
+		elif Input.is_action_pressed("ui_left") and resultLeft.empty():
 			moving = true
 			direction = Vector2(-1, 0)
 			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_right"):
+		elif Input.is_action_pressed("ui_right") and resultRight.empty():
 			moving = true
 			direction = Vector2(1, 0)
 			startPosition = get_pos()
