df7e26856fa89df5003741163a075ee837d25d80
Ryan Hennings <r.hennings11@gmail.com>
2017-07-01 17:57:12 -0700

    animated walking

--- Scripts/hero.gd
+++ Scripts/hero.gd
@@ -9,9 +9,14 @@ const GRID = 16
 
 var world
 
+var sprite
+var animationPlayer
+
 func _ready():
 	world = get_world_2d().get_direct_space_state()
 	set_fixed_process(true)
+	sprite = get_node("Sprite")
+	animationPlayer = get_node("AnimationPlayer")
 
 func _fixed_process(delta):
 	if !moving:
@@ -19,23 +24,34 @@ func _fixed_process(delta):
 		var resultDown = world.intersect_point(get_pos() + Vector2(0, GRID))
 		var resultLeft = world.intersect_point(get_pos() + Vector2(-GRID, 0))
 		var resultRight = world.intersect_point(get_pos() + Vector2(GRID, 0))
-		
-		if Input.is_action_pressed("ui_up") and resultUp.empty():
-			moving = true
-			direction = Vector2(0, -1)
-			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_down") and resultDown.empty():
-			moving = true
-			direction = Vector2(0, 1)
-			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_left") and resultLeft.empty():
-			moving = true
-			direction = Vector2(-1, 0)
-			startPosition = get_pos()
-		elif Input.is_action_pressed("ui_right") and resultRight.empty():
-			moving = true
-			direction = Vector2(1, 0)
-			startPosition = get_pos()
+		if Input.is_action_pressed("ui_up"):
+			sprite.set_frame(10)
+			if resultUp.empty():
+				moving = true
+				direction = Vector2(0, -1)
+				startPosition = get_pos()
+				animationPlayer.play("walk_up")
+		elif Input.is_action_pressed("ui_down"):
+			sprite.set_frame(1)
+			if resultDown.empty():
+				moving = true
+				direction = Vector2(0, 1)
+				startPosition = get_pos()
+				animationPlayer.play("walk_down")
+		elif Input.is_action_pressed("ui_left"):
+			sprite.set_frame(4)
+			if resultLeft.empty():
+				moving = true
+				direction = Vector2(-1, 0)
+				startPosition = get_pos()
+				animationPlayer.play("walk_left")
+		elif Input.is_action_pressed("ui_right"):
+			sprite.set_frame(7)
+			if resultRight.empty():
+				moving = true
+				direction = Vector2(1, 0)
+				startPosition = get_pos()
+				animationPlayer.play("walk_right")
 	else:
 		move_to(get_pos() + direction * SPEED)
 		if get_pos() == startPosition + Vector2(GRID * direction.x, GRID * direction.y):
