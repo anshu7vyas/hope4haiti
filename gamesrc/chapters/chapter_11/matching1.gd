cb07b9fcc4cccf367da9515f3aab3657d15e4541
av-7 <amanshulvyas@gmail.com>
2017-09-01 18:18:00 -0700

    skeleton for fill in the blanks ch11 done

--- gamesrc/chapters/chapter_11/matching1.gd
+++ gamesrc/chapters/chapter_11/matching1.gd
@@ -15,40 +15,40 @@ func _ready():
 
 func check_answers():
 	get_node("correct_answers").set_hidden(false)
-	if get_node("answers/fill_in").get_text() != "nous":
+	if get_node("answers/fill_in").get_text() != "woke up":
 		get_parent().chapter_score -= 4
-		get_node("correct_answers/corrent_answer3").set_hidden(false)
+		get_node("correct_answers/corrent_answer1").set_hidden(false)
 		get_node("marks/marker1").set_texture(load("res://chapters/chapter_04/red_cross.png"))
 	else:
-		get_node("correct_answers/corrent_answer3").set_hidden(true)
+		get_node("correct_answers/corrent_answer1").set_hidden(true)
 		
-	if get_node("answers/fill_in1").get_text() != "tu/vous":
+	if get_node("answers/fill_in1").get_text() != "walked":
 		get_parent().chapter_score -= 4
 		get_node("correct_answers/corrent_answer2").set_hidden(false)
 		get_node("marks/marker2").set_texture(load("res://chapters/chapter_04/red_cross.png"))
 	else:
 		get_node("correct_answers/corrent_answer2").set_hidden(true)
 		
-	if get_node("answers/fill_in2").get_text() != "je":
+	if get_node("answers/fill_in2").get_text() != "witnessed":
 		get_parent().chapter_score -= 4
-		get_node("correct_answers/corrent_answer1").set_hidden(false)
+		get_node("correct_answers/corrent_answer3").set_hidden(false)
 		get_node("marks/marker3").set_texture(load("res://chapters/chapter_04/red_cross.png"))
 	else:
-		get_node("correct_answers/corrent_answer1").set_hidden(true)
+		get_node("correct_answers/corrent_answer3").set_hidden(true)
 		
-	if get_node("answers/fill_in3").get_text() != "ils/elles":
-		get_parent().chapter_score -= 4
-		get_node("correct_answers/corrent_answer5").set_hidden(false)
-		get_node("marks/marker4").set_texture(load("res://chapters/chapter_04/red_cross.png"))
-	else:
-		get_node("correct_answers/corrent_answer5").set_hidden(true)
-		
-	if get_node("answers/fill_in4").get_text() != "il/elle":
+	if get_node("answers/fill_in3").get_text() != "gathered":
 		get_parent().chapter_score -= 4
 		get_node("correct_answers/corrent_answer4").set_hidden(false)
-		get_node("marks/marker5").set_texture(load("res://chapters/chapter_04/red_cross.png"))
+		get_node("marks/marker4").set_texture(load("res://chapters/chapter_04/red_cross.png"))
 	else:
 		get_node("correct_answers/corrent_answer4").set_hidden(true)
+		
+	if get_node("answers/fill_in4").get_text() != "spoke":
+		get_parent().chapter_score -= 4
+		get_node("correct_answers/corrent_answer5").set_hidden(false)
+		get_node("marks/marker5").set_texture(load("res://chapters/chapter_04/red_cross.png"))
+	else:
+		get_node("correct_answers/corrent_answer5").set_hidden(true)
 
 
 func _fixed_process(delta):
@@ -67,10 +67,10 @@ func _fixed_process(delta):
 		answers.get_node("fill_in").set_text(b1array.get_node("Button_tu").get_text())
 	elif b1array.get_node("Button_nous").is_pressed():
 		answers.get_node("fill_in").set_text(b1array.get_node("Button_nous").get_text())
-	elif b1array.get_node("Button_heshe").is_pressed():
-		answers.get_node("fill_in").set_text(b1array.get_node("Button_heshe").get_text())
-	elif b1array.get_node("Button_they").is_pressed():
-		answers.get_node("fill_in").set_text(b1array.get_node("Button_they").get_text())
+	#elif b1array.get_node("Button_heshe").is_pressed():
+	#	answers.get_node("fill_in").set_text(b1array.get_node("Button_heshe").get_text())
+	#elif b1array.get_node("Button_they").is_pressed():
+	#	answers.get_node("fill_in").set_text(b1array.get_node("Button_they").get_text())
 
 	#second row matching
 	if b2array.get_node("Button_je").is_pressed():
@@ -79,10 +79,10 @@ func _fixed_process(delta):
 		answers.get_node("fill_in1").set_text(b2array.get_node("Button_tu").get_text())
 	elif b2array.get_node("Button_nous").is_pressed():
 		answers.get_node("fill_in1").set_text(b2array.get_node("Button_nous").get_text())
-	elif b2array.get_node("Button_heshe").is_pressed():
-		answers.get_node("fill_in1").set_text(b2array.get_node("Button_heshe").get_text())
-	elif b2array.get_node("Button_they").is_pressed():
-		answers.get_node("fill_in1").set_text(b2array.get_node("Button_they").get_text())
+	#elif b2array.get_node("Button_heshe").is_pressed():
+	#	answers.get_node("fill_in1").set_text(b2array.get_node("Button_heshe").get_text())
+	#elif b2array.get_node("Button_they").is_pressed():
+	#	answers.get_node("fill_in1").set_text(b2array.get_node("Button_they").get_text())
 
 	#third row matching
 	if b3array.get_node("Button_je").is_pressed():
@@ -91,10 +91,10 @@ func _fixed_process(delta):
 		answers.get_node("fill_in2").set_text(b3array.get_node("Button_tu").get_text())
 	elif b3array.get_node("Button_nous").is_pressed():
 		answers.get_node("fill_in2").set_text(b3array.get_node("Button_nous").get_text())
-	elif b3array.get_node("Button_heshe").is_pressed():
-		answers.get_node("fill_in2").set_text(b3array.get_node("Button_heshe").get_text())
-	elif b3array.get_node("Button_they").is_pressed():
-		answers.get_node("fill_in2").set_text(b3array.get_node("Button_they").get_text())
+	#elif b3array.get_node("Button_heshe").is_pressed():
+	#	answers.get_node("fill_in2").set_text(b3array.get_node("Button_heshe").get_text())
+	#elif b3array.get_node("Button_they").is_pressed():
+	#	answers.get_node("fill_in2").set_text(b3array.get_node("Button_they").get_text())
 
 	#fourth row matching
 	if b4array.get_node("Button_je").is_pressed():
@@ -103,10 +103,10 @@ func _fixed_process(delta):
 		answers.get_node("fill_in3").set_text(b4array.get_node("Button_tu").get_text())
 	elif b4array.get_node("Button_nous").is_pressed():
 		answers.get_node("fill_in3").set_text(b4array.get_node("Button_nous").get_text())
-	elif b4array.get_node("Button_heshe").is_pressed():
-		answers.get_node("fill_in3").set_text(b4array.get_node("Button_heshe").get_text())
-	elif b4array.get_node("Button_they").is_pressed():
-		answers.get_node("fill_in3").set_text(b4array.get_node("Button_they").get_text())
+	#elif b4array.get_node("Button_heshe").is_pressed():
+	#	answers.get_node("fill_in3").set_text(b4array.get_node("Button_heshe").get_text())
+	#elif b4array.get_node("Button_they").is_pressed():
+	#	answers.get_node("fill_in3").set_text(b4array.get_node("Button_they").get_text())
 
 	#fifth row matching
 	if b5array.get_node("Button_je").is_pressed():
@@ -115,8 +115,8 @@ func _fixed_process(delta):
 		answers.get_node("fill_in4").set_text(b5array.get_node("Button_tu").get_text())
 	elif b5array.get_node("Button_nous").is_pressed():
 		answers.get_node("fill_in4").set_text(b5array.get_node("Button_nous").get_text())
-	elif b5array.get_node("Button_heshe").is_pressed():
-		answers.get_node("fill_in4").set_text(b5array.get_node("Button_heshe").get_text())
-	elif b5array.get_node("Button_they").is_pressed():
-		answers.get_node("fill_in4").set_text(b5array.get_node("Button_they").get_text())
+	#elif b5array.get_node("Button_heshe").is_pressed():
+	#	answers.get_node("fill_in4").set_text(b5array.get_node("Button_heshe").get_text())
+	#elif b5array.get_node("Button_they").is_pressed():
+	#	answers.get_node("fill_in4").set_text(b5array.get_node("Button_they").get_text())
 	
